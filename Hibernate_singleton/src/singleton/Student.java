5b40268eb97b6d4b8d1e5366ac00fe4f9acbd519
JayapradaBiswal <jayaprada.nalanda@gmail.com>
2021-01-06 22:18:22 +0530

    done

---
+++ Hibernate_singleton/src/singleton/Student.java
@@ -0,0 +1,20 @@
+package singleton;
+
+public class Student
+{
+private  final static Student s;
+  static
+    {
+	s=new Student();
+	}
+
+private Student()
+{
+	super();
+}
+public final synchronized static Student studentFactory()
+{
+	return s;
+	
+}
+}
